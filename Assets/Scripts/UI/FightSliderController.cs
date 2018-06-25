298b280795598578a85c093fa201dfb4abf40cc2
Marco Marchitelli <marchitelli97@libero.it>
2018-06-25 03:14:17 +0200

    Started making the fight not look bad :)

--- Assets/Scripts/UI/FightSliderController.cs
+++ Assets/Scripts/UI/FightSliderController.cs
@@ -1,22 +1,70 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
+using System.Collections;
 
 public class FightSliderController : MonoBehaviour {
 
-    public Image blueRayImage;
+    public Slider FightSlider;
+    public Image defRayImage;
+    public Animator defRayAnimator;
+    public Image atkRayImage;
+    public Animator atkRayAnimator;
+    public Image Handle;
+    public Sprite[] HandleCombinations;
 
-    public void SetBlueRayFill(float sliderValue)
+    public UnityEvent OnRaysAnimationEnd;
+
+    bool inFight = false;
+
+    public void InFight(bool flag)
     {
-        float sliderValuePercent = GetSliderValuePercent(sliderValue);
-        blueRayImage.fillAmount = 1-sliderValuePercent;
+        inFight = flag;
     }
-	
+
+    public void SetDefRayFill(float sliderValue)
+    {
+        if (inFight)
+        {
+            float sliderValuePercent = GetSliderValuePercent(sliderValue);
+            defRayImage.fillAmount = 1 - sliderValuePercent;
+        }
+    }
+
     public float GetSliderValuePercent(float _sliderValue)
     {
         float sliderLenght = CombatManager.instance.maxValue + Mathf.Abs(CombatManager.instance.minValue);
         float percent = Mathf.Abs(_sliderValue) * 100 / sliderLenght;
 
         return percent/100; 
+    }
+
+    public void StartRaysAnimation(float duration)
+    {
+        StartCoroutine(RaysAnimation(duration));
+    }
+
+    IEnumerator RaysAnimation(float duration)
+    {
+        float timer = 0;
+        float atkRayTarget = GetSliderValuePercent(FightSlider.value);
+        float defRayTarget = 1 - GetSliderValuePercent(FightSlider.value);
+        float atkRayStart = atkRayImage.fillAmount;
+        float defRayStart = defRayImage.fillAmount;
+
+        atkRayAnimator.SetTrigger("Start");
+        defRayAnimator.SetTrigger("Start");
+
+        while(timer <= duration)
+        {
+            timer += Time.deltaTime;
+            atkRayImage.fillAmount = Mathf.Lerp(atkRayStart, atkRayTarget, timer);
+            defRayImage.fillAmount = Mathf.Lerp(defRayStart, defRayTarget, timer);
+            yield return null;
+        }
+
+        InFight(true);
+        OnRaysAnimationEnd.Invoke();
     }
 
 }
