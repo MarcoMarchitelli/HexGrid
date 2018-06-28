77acc6912dcf8348a8c98b8269b3cdaa46023cd1
Marco Marchitelli <marchitelli97@libero.it>
2018-06-28 21:12:58 +0200

    Aggiunto help in basso a destra, Riorganizzate alcune cartelle.

---
+++ Assets/Scripts/Managers/HelpManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HelpManager : MonoBehaviour {
+
+    public Sprite[] HelpSlides;
+    public Image SlidesVisualizer;
+    public GameObject HelpPanel;
+
+    [Header("Nav Buttons")]
+    public GameObject PrevButton;
+    public GameObject NextButton;
+
+    [Header("Texts")]
+    public TextMeshProUGUI CurrentSlideNumber;
+    public TextMeshProUGUI MaxSlideNumber;
+
+    int slideIndex = 0;
+
+    public void Refresh()
+    {
+        //refresh slide
+        SlidesVisualizer.sprite = HelpSlides[slideIndex];
+
+        //refresh nav buttons
+        if (slideIndex == 0)
+            PrevButton.SetActive(false);
+        else if (slideIndex == HelpSlides.Length - 1)
+            NextButton.SetActive(false);
+        else
+        {
+            PrevButton.SetActive(true);
+            NextButton.SetActive(true);
+        }
+
+        CurrentSlideNumber.text = (slideIndex + 1).ToString();
+        MaxSlideNumber.text = HelpSlides.Length.ToString();
+
+    }
+
+    public void HelpToggle()
+    {
+        if (HelpPanel.activeSelf)
+            HelpPanel.SetActive(false);
+        else
+            HelpPanel.SetActive(true);
+
+        slideIndex = 0;
+
+        Refresh();
+    }
+	
+    public void NextSlide()
+    {
+        if (slideIndex != HelpSlides.Length - 1)
+            slideIndex++;
+
+        Refresh();
+    }
+
+    public void PreviousSlide()
+    {
+        if (slideIndex != 0)
+            slideIndex--;
+
+        Refresh();
+    }
+
+}
